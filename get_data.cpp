44f6a54ac11412e7543a381b0c6026e6a67742df
blairfix <blairfix@gmail.com>
2022-05-30 14:53:30 -0400

    updated get data using boost and rcpp id

--- get_data.cpp
+++ get_data.cpp
@@ -1,9 +1,9 @@
 #include <Rcpp.h>
 #include <fstream>
 #include <string>
+#include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string.hpp>
 #include <vector>
-
 
 using namespace Rcpp;
 
@@ -11,12 +11,14 @@ using namespace Rcpp;
 // [[Rcpp::plugins(cpp11)]]
 // [[Rcpp::export]]
 
-DataFrame get_data(std::string filename,
-                   std::vector<int> ids
+
+DataFrame get_data_test(
+			std::string filename,
+			Rcpp::IntegerVector ids
                    )
 {
 
-    // test if file exists
+    // 1. test if file exists
     std::ifstream ifile;
     ifile.open(filename);
     if(ifile) {
@@ -30,17 +32,13 @@ DataFrame get_data(std::string filename,
     }
 
 
-    // read in ngram data
+    // 2. read in ngram data
     // ****************************************************
-    int global_counter = 0;
 
     // output vectors
-    int file_guess = 50000000;
-
-    std::vector<std::string> word_vec(file_guess);
-    std::vector<double> year_vec(file_guess);
-    std::vector<double> wordcount_vec(file_guess);
-
+    std::list<std::string> word_vec;
+    std::list<double> year_vec;
+    std::list<double> wordcount_vec;
 
     // read in ngram
     std::string ngram;
@@ -51,100 +49,41 @@ DataFrame get_data(std::string filename,
 
     while (getline(in, ngram)) {
 
-
         // test if line in id
         if( line_number == ids[id_counter] ){
+
             id_counter++;
 
-            // find first tab
-            std::string tab = "\t";
-            size_t id_tab = ngram.find(tab);
+            // split line on tab
+            std::vector<std::string> line_split;
+            boost::split(line_split, ngram, boost::is_any_of("\t") );
 
-            // get word
-            if( id_tab != std::string::npos ){
+            if(line_split.size() > 1){
 
-                std::string word = ngram.substr(0, id_tab);
+                // make word lower case
+                boost::to_lower( line_split[0] );
 
-                // convert word to lower case
-                boost::to_lower(word);
+                // loop over data
+                for(int i = 1; i < line_split.size(); i++){
 
-                // erase word from ngram
-                ngram.erase(0, id_tab + tab.length());
+                    // split data on comma
+                    std::vector<std::string> data;
+                    boost::split(data,  line_split[i], boost::is_any_of(",") );
 
-                // split on tab
-                std::string delimiter = "\t";
-                size_t pos = 0;
-                std::string subdata;
-
-                while ((pos = ngram.find(delimiter)) != std::string::npos) {
-
-                    // word
-                    word_vec[global_counter] = word;
-
-                    // get year and word count
-                    subdata = ngram.substr(0, pos);
-
-                    // year
-                    size_t comma1 = subdata.find(",");
-                    std::string year = subdata.substr(0, comma1);
-                    year_vec[global_counter] = std::stoi(year);
-
-                    // word count
-                    subdata.erase(0, comma1 + 1);
-                    size_t comma2 = subdata.find(",");
-                    std::string wordcount = subdata.substr(0, comma2);
-                    wordcount_vec[global_counter] = std::stoi(wordcount);
-
-                    // erase tab
-                    ngram.erase(0, pos + delimiter.length());
-
-                    // advance global counter
-                    global_counter++;
+                    // append data
+                    word_vec.push_back( line_split[0] );
+                    year_vec.push_back( std::stoi( data[0] ) );
+                    wordcount_vec.push_back( std::stoi( data[1] ) );
 
                 }
-
-                // get last entry
-                word_vec[global_counter] = word;
-
-                size_t comma1 = ngram.find(",");
-                std::string year = ngram.substr(0, comma1);
-                year_vec[global_counter] = std::stoi(year);
-
-                ngram.erase(0, comma1 + 1);
-                size_t comma2 = ngram.find(",");
-                std::string wordcount = ngram.substr(0, comma2);
-                wordcount_vec[global_counter] = std::stoi(wordcount);
-
-                // advance global counter
-                global_counter++;
-
             }
-
-
-            if(global_counter == word_vec.size()){
-
-                std::vector<std::string> B(5000000);
-                word_vec.insert(word_vec.end(), B.begin(), B.end());
-
-                std::vector<double> C(5000000);
-                year_vec.insert(year_vec.end(), C.begin(), C.end());
-                wordcount_vec.insert(wordcount_vec.end(), C.begin(), C.end());
-
-            }
-
         }
 
         line_number++;
-
     }
 
-    // erase zeros
-    word_vec.erase (word_vec.begin() + global_counter, word_vec.end() );
-    year_vec.erase (year_vec.begin() + global_counter, year_vec.end() );
-    wordcount_vec.erase (wordcount_vec.begin() + global_counter, wordcount_vec.end() );
 
-
-    // bind output in dataframe
+    // 3. bind output in dataframe
     DataFrame output = DataFrame::create(
         Named("word") =      word_vec,
         Named("year") =      year_vec,
