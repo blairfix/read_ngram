ff4a55d1381030e9575f8bc95f5fe0b1e7a59e1c
blairfix <blairfix@fastmail.com>
2025-03-05 16:33:38 -0700

    updated get_data with simpler code

--- get_data.cpp
+++ get_data.cpp
@@ -1,9 +1,9 @@
 #include <Rcpp.h>
 #include <fstream>
 #include <string>
+#include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string.hpp>
 #include <vector>
-
 
 using namespace Rcpp;
 
@@ -11,35 +11,34 @@ using namespace Rcpp;
 // [[Rcpp::plugins(cpp11)]]
 // [[Rcpp::export]]
 
-DataFrame get_data(std::string filename,
-		   IntegerVector ids
-                   )
+
+DataFrame get_data(
+	std::string filename,
+	Rcpp::IntegerVector ids
+	)
 {
 
-    // test if file exists
+    // 1. test if file exists
     std::ifstream ifile;
     ifile.open(filename);
     if(ifile) {
     } else {
-        throw std::range_error("File does not exist");
+	throw std::range_error("File does not exist");
     }
 
 
     if( ids.size() == 0 ){
-        throw std::range_error("ids vector is blank");
+	throw std::range_error("ids vector is blank");
     }
 
 
-    // read in ngram data
-    // ****************************************************
-    int global_counter = 0;
+    // 2. read in ngram data
+    // -----------------------------------------------------
 
     // output vectors
-    int file_guess = 50000000;
-
-    std::vector<std::string> word_vec(file_guess);
-    std::vector<double> year_vec(file_guess);
-    std::vector<double> wordcount_vec(file_guess);
+    std::deque<std::string> word_vec;
+    std::deque<double> year_vec;
+    std::deque<double> wordcount_vec;
 
 
     // read in ngram
@@ -49,108 +48,61 @@ DataFrame get_data(std::string filename,
     int line_number = 1;
     int id_counter = 0;
 
-    while (getline(in, ngram)) {
+    // get number of words
+    int nwords = ids.size();
 
+    // read in file and get data with rows in id vector
+    while (getline(in, ngram) ) {
 
-        // test if line in id
-        if( line_number == ids[id_counter] ){
-            id_counter++;
+	// test if id counter still within bounds
+	if(  id_counter < nwords ){
 
-            // find first tab
-            std::string tab = "\t";
-            size_t id_tab = ngram.find(tab);
+	    // test if we want this ngram line
+	    if( line_number == ids[id_counter] ){
 
-            // get word
-            if( id_tab != std::string::npos ){
+		// advance word counter
+		id_counter++;
 
-                std::string word = ngram.substr(0, id_tab);
+		// split line on tab
+		std::vector<std::string> line_split;
+		boost::split(line_split, ngram, boost::is_any_of("\t") );
 
-                // convert word to lower case
-                boost::to_lower(word);
+		if( line_split.size() > 1 ){
 
-                // erase word from ngram
-                ngram.erase(0, id_tab + tab.length());
+		    // make word lower case
+		    boost::to_lower( line_split[0] );
 
-                // split on tab
-                std::string delimiter = "\t";
-                size_t pos = 0;
-                std::string subdata;
+		    // loop over data
+		    for( int i = 1; i < line_split.size(); i++){
 
-                while ((pos = ngram.find(delimiter)) != std::string::npos) {
+			// split data on comma
+			std::vector<std::string> data;
+			boost::split(data,  line_split[i], boost::is_any_of(",") );
 
-                    // word
-                    word_vec[global_counter] = word;
+			// append data
+			word_vec.emplace_back( line_split[0] );
+			year_vec.emplace_back( std::stoi( data[0] ) );
+			wordcount_vec.emplace_back( std::stoi( data[1] ) );
 
-                    // get year and word count
-                    subdata = ngram.substr(0, pos);
+		    }
+		}
+	    }
+	}
 
-                    // year
-                    size_t comma1 = subdata.find(",");
-                    std::string year = subdata.substr(0, comma1);
-                    year_vec[global_counter] = std::stoi(year);
-
-                    // word count
-                    subdata.erase(0, comma1 + 1);
-                    size_t comma2 = subdata.find(",");
-                    std::string wordcount = subdata.substr(0, comma2);
-                    wordcount_vec[global_counter] = std::stoi(wordcount);
-
-                    // erase tab
-                    ngram.erase(0, pos + delimiter.length());
-
-                    // advance global counter
-                    global_counter++;
-
-                }
-
-                // get last entry
-                word_vec[global_counter] = word;
-
-                size_t comma1 = ngram.find(",");
-                std::string year = ngram.substr(0, comma1);
-                year_vec[global_counter] = std::stoi(year);
-
-                ngram.erase(0, comma1 + 1);
-                size_t comma2 = ngram.find(",");
-                std::string wordcount = ngram.substr(0, comma2);
-                wordcount_vec[global_counter] = std::stoi(wordcount);
-
-                // advance global counter
-                global_counter++;
-
-            }
-
-	    // if at the end of output container, append more rows
-            if(global_counter == word_vec.size()){
-
-                std::vector<std::string> B(5000000);
-                word_vec.insert(word_vec.end(), B.begin(), B.end());
-
-                std::vector<double> C(5000000);
-                year_vec.insert(year_vec.end(), C.begin(), C.end());
-                wordcount_vec.insert(wordcount_vec.end(), C.begin(), C.end());
-
-            }
-
-        }
-
-        line_number++;
-
+	line_number++;
     }
 
-    // erase zeros at end of container
-    word_vec.erase (word_vec.begin() + global_counter, word_vec.end() );
-    year_vec.erase (year_vec.begin() + global_counter, year_vec.end() );
-    wordcount_vec.erase (wordcount_vec.begin() + global_counter, wordcount_vec.end() );
 
+    // 3. bind output in dataframe
+    // -----------------------------------------------------
 
-    // bind output in dataframe
     DataFrame output = DataFrame::create(
-        Named("word") =      word_vec,
-        Named("year") =      year_vec,
-        Named("wordcount") = wordcount_vec
-    );
+	    Named("word") =      word_vec,
+	    Named("year") =      year_vec,
+	    Named("wordcount") = wordcount_vec
+	    );
 
     return output;
 
 }
+
